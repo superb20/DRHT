59d4e0d7085b42129559503cd35f7d7065d4ef99
superb20 <superb20.lee@gmail.com>
2019-01-22 01:58:31 +0900

    refactoring evaluate_mobilenet

--- ide/vs2017/NIMA/evaluate_mobilenet.py
+++ ide/vs2017/NIMA/evaluate_mobilenet.py
@@ -15,20 +15,15 @@ from score_utils import mean_score, std_score
 import argparse
 import glob
 
-def get_image_list(args):
-    # give priority to directory
-    if args.dir is not None:
-        print("Loading images from directory : ", args.dir)
-        images = glob.glob(args.dir + '/*.png')
-        images += glob.glob(args.dir + '/*.jpg')
-        images += glob.glob(args.dir + '/*.jpeg')
-
-    elif args.img[0] is not None:
-        print("Loading images from path(s) : ", args.img)
-        images = args.img
+def get_image_list(image_dir):
+    if image_dir is not None:
+        print("Loading images from directory : ", image_dir)
+        images = glob.glob(image_dir + '/*.png')
+        images += glob.glob(image_dir + '/*.jpg')
+        images += glob.glob(image_dir + '/*.jpeg')
 
     else:
-        raise RuntimeError('Either -dir or -img arguments must be passed as argument')
+        raise RuntimeError('Either -dir arguments must be passed as argument')
 
     return images
 
@@ -41,50 +36,72 @@ def preprocess_img(img):
     x = preprocess_input(x)
     return x
 
-# Set argument parser
-parser = argparse.ArgumentParser(description='Evaluate NIMA(MobileNet)')
-parser.add_argument('-dir', type=str, default=None, help='Pass a directory to evaluate the images in it')
-parser.add_argument('-img', type=str, default=[None], nargs='+', help='Pass one or more image paths to evaluate them')
-parser.add_argument('-resize', type=str, default='false', help='Resize images to 224x224 before scoring')
-parser.add_argument('-rank', type=str, default='true', help='Whether to tank the images after they have been scored')
+def set_argument_parser():
+    parser = argparse.ArgumentParser(description='Evaluate NIMA(MobileNet)')
+    parser.add_argument('-dir', type=str, default=None, help='Pass a directory to evaluate the images in it')
+    parser.add_argument('-resize', type=str, default='false', help='Resize images to 224x224 before scoring')
+    parser.add_argument('-rank', type=str, default='true', help='Whether to tank the images after they have been scored')
+    
+    return parser
 
-# Parse arguments
-args = parser.parse_args()
-resize_image = args.resize.lower() in ('true', 'yes', 't', '1')
-target_size = (224, 224) if resize_image else None
-rank_images = args.rank.lower() in ("true", "yes", "t", "1")
+def parse_argument_parser(parser):
+    args = parser.parse_args()
+    resize_image = args.resize.lower() in ('true', 'yes', 't', '1')
+    target_size = (224, 224) if resize_image else None
+    rank_images = args.rank.lower() in ("true", "yes", "t", "1")
+    image_dir = args.dir
 
-# Print configs
-print("image resize      :", resize_image)
-print("image target_size :", target_size)
-print("print ranking     :", rank_images)
+    # Print configs
+    print("image resize      :", resize_image)
+    print("image target_size :", target_size)
+    print("print ranking     :", rank_images)
 
-images = get_image_list(args)
+    return resize_image, target_size, rank_images, image_dir
 
-# Set model
-base_model = MobileNet((None, None, 3), alpha=1, include_top=False, pooling='avg', weights=None)
-x = Dropout(0.75)(base_model.output)
-x = Dense(10, activation='softmax')(x)
-model = Model(base_model.input, x)
-model.load_weights('weights/mobilenet_weights.h5')
+def set_model(weight):
+    base_model = MobileNet((None, None, 3), alpha=1, include_top=False, pooling='avg', weights=None)
+    x = Dropout(0.75)(base_model.output)
+    x = Dense(10, activation='softmax')(x)
+    model = Model(base_model.input, x)
+    model.load_weights(weight)
+    return model
 
-# prediction score
-score_list = []
+def prediction_score(images, target_size):
+    # prediction score
+    score_list = []
 
-for img_path in images:
-    print("Evaluating : ", img_path)
-    img = load_img(img_path, target_size=target_size)
-    img = preprocess_img(img)
-    scores = model.predict(img, batch_size=1, verbose=0)[0]
+    for img_path in images:
+        print("Evaluating : ", img_path)
+        img = load_img(img_path, target_size=target_size)
+        img = preprocess_img(img)
+        scores = model.predict(img, batch_size=1, verbose=0)[0]
 
-    mean, std = get_score(scores)
-    score_list.append((img_path, mean))
-    print("NIMA Score : %0.3f +- (%0.3f)" % (mean, std) + "\n")
+        mean, std = get_score(scores)
+        score_list.append((img_path, mean))
+        print("NIMA Score : %0.3f +- (%0.3f)" % (mean, std) + "\n")
 
-# rank images
-if rank_images:
+    return score_list
+
+def ranking_score(score_list):
     print("[Ranking Images]")
+    rank_list = []
+
     score_list = sorted(score_list, key=lambda x: x[1], reverse=True)
 
     for i, (name, score) in enumerate(score_list):
-        print("[%3d]" % (i + 1), "%50s : Score = %0.5f" % (name, score))
+        print("[%3d]" % (i + 1), "%-35
+        s : Score = %0.5f" % (name, score))
+        rank_list.append((name, score))
+
+if __name__== "__main__":
+    parser = set_argument_parser()    
+    resize_image, target_size, rank_images, image_dir = parse_argument_parser(parser)
+    images = get_image_list(image_dir)
+    model = set_model('weights/mobilenet_weights.h5')
+
+    score_list = prediction_score(images, target_size)
+
+    # rank images
+    if rank_images:
+        rank_list = ranking_score(score_list)
+        
