eb7206e4905e4eb08c79673683516d926478b75e
superb20 <superb20.lee@gmail.com>
2019-02-27 01:34:55 +0900

    init tensorflow lite

--- applications/Nima/app/src/main/java/com/superb20/nima/MainFragment.java
+++ applications/Nima/app/src/main/java/com/superb20/nima/MainFragment.java
@@ -3,6 +3,7 @@ package com.superb20.nima;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +12,40 @@ import android.view.ViewGroup;
  * Created by Superb20 on 2019-02-14.
  */
 
+// https://codelabs.developers.google.com/codelabs/tensorflow-for-poets-2-tflite/#3
+
 public class MainFragment extends Fragment {
     private final static String TAG = "MainFragment";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate()");
     }
 
+    /**
+     * Layout the preview and buttons.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_main, parent, false);
-        return v;
+        Log.i(TAG, "onCreateView()");
+        return inflater.inflate(R.layout.fragment_main, parent, false);
+    }
+
+    /**
+     * Connect the buttons to their event handler.
+     */
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        Log.i(TAG, "onViewCreated()");
+    }
+
+    /**
+     * Load the model and labels.
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.i(TAG, "onActivityCreated()");
     }
 }
